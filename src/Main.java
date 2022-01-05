0d7cfd79c598180ca4efd13d7d14b4040f12a9e5
Antoine Buquet <antoine.buquet49@gmail.com>
2022-01-05 14:24:56 +0100

    int to binary supporte les negatif

--- src/Main.java
+++ src/Main.java
@@ -276,15 +276,53 @@ public class Main {
     }
 
     private static String intToBinary(String strInt, int bits) {
-        String strWithoutFirstChar = strInt.substring(1);
-        int Int = Integer.parseInt(strWithoutFirstChar);
-        StringBuilder binStr = new StringBuilder(Integer.toBinaryString(Int));
-        if (bits != 0) {
-            while (binStr.length() != bits) {
-                binStr.insert(0, "0");
+        if (strInt.charAt(0) == '-'){
+            String positiveStrInt = intToBinary(strInt.substring(1), bits);
+            return twosCompliment(positiveStrInt);
+        } else {
+            if (strInt.charAt(0) == 'r' || strInt.charAt(0) == '#'){
+                strInt = strInt.substring(1);
+            }
+            int Int = Integer.parseInt(strInt);
+            StringBuilder binStr = new StringBuilder(Integer.toBinaryString(Int));
+            if (bits != 0) {
+                while (binStr.length() != bits) {
+                    binStr.insert(0, "0");
+                }
+            }
+            return binStr.toString();
+        }
+    }
+
+    public static String twosCompliment(String bin) {
+        String twos = "", ones = "";
+
+        for (int i = 0; i < bin.length(); i++) {
+            ones += flip(bin.charAt(i));
+        }
+        int number0 = Integer.parseInt(ones, 2);
+        StringBuilder builder = new StringBuilder(ones);
+        boolean b = false;
+        for (int i = ones.length() - 1; i > 0; i--) {
+            if (ones.charAt(i) == '1') {
+                builder.setCharAt(i, '0');
+            } else {
+                builder.setCharAt(i, '1');
+                b = true;
+                break;
             }
         }
-        return binStr.toString();
+        if (!b)
+            builder.append("1", 0, 7);
+
+        twos = builder.toString();
+
+        return twos;
+    }
+
+    // Returns '0' for '1' and '1' for '0'
+    public static char flip(char c) {
+        return (c == '0') ? '1' : '0';
     }
 
     private static String intToBinaryDividedBy4(String strInt, int bits) {
